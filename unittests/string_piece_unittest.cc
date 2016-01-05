0c9326ce82a00c573128dd7251858fd29dd9914b
wulaxx <xu_chu@163.com>
2016-01-05 16:08:28 +0800

    修改unittest的缩进

---
+++ unittests/string_piece_unittest.cc
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include <base/string_piece.h>
+#include <string>
+using namespace std;
+using namespace cnetpp::base;
+
+TEST(StringPiece, MiscTest)
+{
+  StringPiece str((const char*)NULL, 0);
+  ASSERT_TRUE(str.empty());
+  string s;
+  str.copy_to_string(&s);
+  ASSERT_EQ(s, "");
+}
+
+TEST(StringPiece, CompareTest)
+{
+  StringPiece str((const char*)NULL, 0);
+  StringPiece str2("", 0);
+  ASSERT_EQ(str, str2);
+  ASSERT_EQ(str2, str2);
+  ASSERT_EQ(str, str);
+  ASSERT_TRUE(str == str2);
+}
+
+TEST(StringPiece, FindTest)
+{
+  StringPiece str("This is a test string");
+  ASSERT_EQ(str.find("is", 0), 2);
+  ASSERT_EQ(str.find("is", 4), 5);
+  ASSERT_EQ(str.find('i', 0), 2);
+  str.set((const char*)NULL, 0);
+  ASSERT_EQ(str.find('c', 9), StringPiece::npos);
+}
