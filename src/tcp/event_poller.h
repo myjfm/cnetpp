7ea1fa36813003261341a9ca024d3492665fb0ed
wulaxx <xu_chu@163.com>
2016-01-06 15:03:45 +0800

    添加select_event_poller_impl文件
    添加event_poller.cc文件
    
    修改event_poller.h的注释以支持doxygen
    
    use SelectEventPollerImpl::InternalFdInfo to hold fd infomation

--- src/tcp/event_poller.h
+++ src/tcp/event_poller.h
@@ -37,45 +37,75 @@ namespace tcp {
 class Command;
 class EventCenter;
 
+/**
+ * @brief Poll io events & commands and dispatch them via EventCenter.
+ * Everty Poller has a poller thread, which call Poll() periodically,
+ * it process events and commands of a galleray of connections.
+ */
 class EventPoller {
  public:
-  // factory function. Create a EventPoller instance.
-  // @param id              the identifier of the EventPoller
-  // @param max_connections the maximum numbers of connections this event poller
-  //                        supports
-  // @return the EventPoller instance
+  /**
+   * factory function. Create a EventPoller instance.
+   * @param id              the identifier of the EventPoller
+   * @param max_connections the maximum numbers of connections this event poller
+   *                        supports
+   * @return the EventPoller instance
+   */
   static std::shared_ptr<EventPoller> New(size_t id,
                                           size_t max_connections = 1024);
   
-  // Initialize the EventPoller.
-  // It includes,
-  //    initilizing the interruptor,
-  //    create an epoll file descriptor(if using epoll), or
-  //    create an event handler(if using MsgWaitForMultipleObjectsEx).
-  //    @param iEventCenter the EventCenter pointer
-  //    @return true if initialization is succeeded, else false.
+  /**
+   * Initialize the EventPoller.
+   * It includes,
+   * - initilizing the interruptor,
+   * - create an epoll file descriptor(if using epoll), or
+   * - create an event handler(if using MsgWaitForMultipleObjectsEx).
+   * @param iEventCenter the EventCenter pointer
+   * @return true if initialization is succeeded, else false.
+   */
   virtual bool Init(std::shared_ptr<EventCenter> event_center) = 0;
   
-  // Wait until some events are ready.
-  // @return false if error occured, else true.
+  /**
+   * @breif Poll avaliable IO event & command.
+   * dispatch events to connection callback via EventCenter::ProcessEvent()
+   * - if interruptor is readable, then process Commands.
+   * - if connection is readable/writeable process Events.
+   * - dispatch events to Connections and invoke event callbacks
+   * @note this function is called within worker thread.
+   * @return false if error occured, else true.
+   */
   virtual bool Poll() = 0;
   
-  // When there are some events need to be added to the EventPoller, or the
-  // EventPoller thread is shutting down, this method will be called.
-  // @return true if the interruption is raised up successfully, else false.
+  /**
+   * @brief Wake up Poll() from waiting so that Poll() can process command
+   * When there are some events need to be added to the EventPoller, or the
+   * EventPoller thread is shutting down, this method will be called.
+   * @return true if the interruption is raised up successfully, else false.
+   */
   virtual bool Interrupt() = 0;
   
-  // Shutdown the EventPoller.
-  // It includes,
-  //    destruct the interruptor,
-  //    cancel all waiting requests,
-  //    close the epoll file descriptor(if using epoll), or
-  //    close the event handler(if using MsgWaitForMultipleObjectsEx).
-  //    @return
+  /**
+   * @brief Shutdown the EventPoller.
+   * It includes,
+   * - destruct the interruptor,
+   * - cancel all waiting requests,
+   * - close the epoll file descriptor(if using epoll), or
+   * - close the event handler(if using MsgWaitForMultipleObjectsEx).
+   */
   virtual void Shutdown() = 0;
 
+  /**
+   * return Id of EventPoller instance
+   * @return Id of EventPoller instance
+   */
   virtual size_t Id() const = 0;
 
+  /**
+   * Process Command from user thread or Connection callbacks.
+   * @param command Command
+   * @return true on success, false on failed
+   * @note this function is called by EventCenter::ProcessAllPendingCommands
+   */
   virtual bool ProcessCommand(const Command& command) = 0;
 };
 
---
+++ src/tcp/select_event_poller_impl.h
@@ -0,0 +1,121 @@
+// Copyright (c) 2015, myjfm(mwxjmmyjfm@gmail.com).  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//   * Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//   * Neither the name of Shuo Chen nor the names of other contributors may be
+// used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+#ifndef __linux__
+#error "Your operating system seems not be linux!"
+#endif
+
+#ifndef CNETPP_TCP_SELECT_EVENT_POLLER_IMPL_H_
+#define CNETPP_TCP_SELECT_EVENT_POLLER_IMPL_H_
+
+#include "event.h"
+#include "event_poller.h"
+
+#include <vector>
+#include <unordered_map>
+
+namespace cnetpp {
+namespace tcp {
+
+class EventCenter;
+
+class SelectEventPollerImpl : public EventPoller {
+ public:
+  explicit SelectEventPollerImpl(int id, size_t max_connections)
+      : id_(id),
+        pipe_read_fd_(-1),
+        pipe_write_fd_(-1),
+        max_connections_(max_connections) {
+		  select_fds_.clear();
+  }
+
+  ~SelectEventPollerImpl() = default;
+
+ protected:
+  bool Init(std::shared_ptr<EventCenter> event_center) override;
+
+  bool Poll() override;
+
+  bool Interrupt() override;
+
+  void Shutdown() override;
+
+  size_t Id() const override {
+    return id_;
+  }
+
+  bool ProcessCommand(const Command& command) override;
+
+ private:
+  int id_; // the id
+  std::weak_ptr<EventCenter> event_center_;
+
+  // used for interrupt the epoll run loop.
+  // We first add the pipe_read_fd_ to the epoll events. When one thread wants
+  // to interrupt the poll thread, we can write a byte to pipe_write_fd_ of the
+  // pipe, the epoll thread will be waken up from epoll_wait()
+  int pipe_read_fd_;
+  int pipe_write_fd_;
+  int max_connections_;
+  class InternalFdInfo {
+    public:
+      enum Type {
+        kSelectNone   = 0x00,
+        kSelectRead   = 0x01,
+        kSelectWrite  = 0x02,
+        kSelectExcept = 0x04
+      };
+      explicit InternalFdInfo(int fd) : 
+        fd_(fd), 
+        mask_(kSelectRead | kSelectExcept) { }
+      InternalFdInfo() : fd_(-1), mask_(kSelectNone) { }
+      ~InternalFdInfo() = default;
+      int fd() const { return fd_;}
+      int GetMask() const { return mask_;}
+      void SetMask(int mask) { mask_ = mask;}
+    private:
+      int fd_;
+      int mask_;
+  };
+
+  std::unordered_map<int, InternalFdInfo> select_fds_;
+
+  bool CreateInterrupter();
+  void DestroyInterrupter();
+
+  bool ProcessInterrupt();
+
+  bool ProcessAddCommand(const Command& command);
+  bool ProcessModifyCommand(const Command& command);
+  bool ProcessRemoveCommand(const Command& command);
+  int BuildFdsets(fd_set* rd_fdset, fd_set* wr_fdset, fd_set* ex_fdset);
+};
+
+}  // namespace tcp
+}  // namespace cnetpp
+
+#endif  // CNETPP_TCP_SELECT_EVENT_POLLER_IMPL_H_
+
